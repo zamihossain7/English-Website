ca5c2a73abdb55b9be6206bdcef45a498fb0e1ca
Zami Hosaain <zhdpark@gmail.com>
2024-02-13 15:22:55 +0600

    first commit

---
+++ english.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>English Parkz</title>
+    <link rel="stylesheet" href="eng.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary navbar bg-dark border-bottom border-body p-0" data-bs-theme="dark">
+        <div class="container">
+          <a class="navbar-brand" href="">
+            <img src="_998ac4c4-b9f3-4e45-92c1-44593eb24507.jpeg" width="50px" alt="">
+
+            English Parkz  
+          </a>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item ml-3">
+                <a class="nav-link x--- " aria-current="page" href="#">Sell Documents</a>
+              </li>
+              <li class="nav-item ml-3">
+                <a class="nav-link x-- " href="#">ইংরেজি সংস্করণ</a>
+              </li>
+              <li class="nav-item ml-3">
+                <a class="nav-link x--" href="#">কোর্সটিকায় লিখুন</a>
+              </li>
+            </ul>
+
+          
+
+          </div>
+        </div>
+   </nav> 
+      <nav class="navbar navbar-expand-lg bg-body-tertiary x-3 nav-underline">
+        <div class="container ">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent" style="justify-content: space-evenly;">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
+              <div class="container-fluid">
+                <div class="dropdown">
+                  <span>Academic <img src="images-removebg-preview.png" width="20px" alt=""></span>
+                  <div class="dropdown-content">
+                    <a href="#">Class Six</a>
+                    <a href="#">Class Seven</a>
+                    <a href="#">Class Eight</a>
+                    <a href="#">Class Nine</a>
+                    <a href="#">Class Ten</a>
+                    <a href="#"> SSC-2024</a>
+                    <a href="#">Class Eleven</a>
+                    <a href="#"> Class Twelve</a>
+                    <a href="#">HSC-2024</a>
+                  </div>
+                </div>
+                <li class="nav-item">
+                  <a class="nav-link text-dark" href="#">National University</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-dark" href="#">Career</a>                
+                </li>              
+                <li class="nav-item">
+                  <a class="nav-link text-dark" href="#">Freelancing</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-dark" href="#">Health Page</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-dark" href="#">Job Circular</a>
+                </li>
+                <li class="nav-item text-dark">
+                  <a class="nav-link text-dark" href="#">Skill development</a>
+                </li>
+                <li class="nav-item text-dark">
+                  <a class="nav-link text-dark" href="#">Scholarship</a>
+                </li>
+                <li class="nav-item text-dark">
+                  <a class="nav-link text-dark" href="#">Learn English</a>
+                </li>
+                <div class="dropdown">
+                  <span>Miscellaneous</span>
+                  <div class="dropdown-content">
+                    <a href="#">Option 1</a>
+                    <a href="#">Option 2</a>
+                    <a href="#">Option 3</a>
+                  </div>
+                </div>
+                <div class="container mt-4">
+                  <div class="dropdown-">
+                    <img src="m2i8K9A0Z5Z5b1m2-removebg-preview.png" width="40px" class=" dropdown-toggle-"  id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <div class="search-container" id="searchContainer">
+                    <input type="text" class="form-control" placeholder="What do you need?">
+                    <button class="btn btn-success mt-2">Search</button>
+                  </div>
+                </div>
+                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <!-- You can add dropdown items here if needed -->
+                  </div>
+              </div>
+              
+                <!-- Search container -->
+          
+              
+            </ul>
+          
+          </div>
+        </div>
+        </nav>
+<h3 class="alert alert-primary" style="height: 50px; font-size: 12px; width:1100px; position:relative; right: -100px; bottom: -30px;" >
+এসএসসি-২০২৪ পরীক্ষা শুরু ১৫ ফেব্রুয়ারি। সকল সাবজেক্টের উত্তরসহ সাজেশন PDF সংগ্রহ করতে <a href="#" style="color: red; text-decoration:none">এই লিংকে ক্লিক</a> করো।</h3>
+
+
+
+
+
+
+      <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+      <!-- Bootstrap JS (Make sure this is included after Bootstrap CSS) -->
+ <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+<!-- Bootstrap JS (Make sure this is included after Bootstrap CSS) -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+</body>
+</html>
+<script>
+  $(document).ready(function(){
+    // When the icon is clicked, toggle the visibility of the search container
+    $('.dropdown-toggle-').click(function(){
+      $('#searchContainer').toggleClass('show');
+    });
+  });
+</script>
+</body>
+</html>
